8ab9b1bd9a54bb1ec1caeb82ec8dbf51db7a4e96
Chad Clark <chadclark12@hotmail.com>
2021-01-14 20:55:58 -0600

    docs:update function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,25 @@
 const APIKey = "3a623ea6ade278ada9b6b26990b8755d";
 
-let cityName = "";
-// URL TO API FOR CURRENT WEATHER BY CITY \\
-const cityQueryURL = `https://api.openweathermap.org/data/2.5/weather?q=minneapolis&appid=${APIKey}`;
+//FUNCTION TO PASS IN CITY NAME FROM LISTENER BELOW\\
+function weatherApp(cityName) {
+  // URL TO API FOR CURRENT WEATHER BY CITY \\
+  const cityQueryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`;
 
-// URL TO API FOR 2HR/5DAY FORECAST BY CITY \\
-const fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=minneapolis&appid=${APIKey}`;
+  // URL TO API FOR 2HR/5DAY FORECAST BY CITY \\
+  const fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
 
-// BUILDING INPUT CITY ELEMENT \\
-let searchCityInputBox = $("<input>");
-searchCityInputBox.addClass("cityNameInput");
-searchCityInputBox.attr("value");
-$(".city").append(searchCityInputBox);
-
-// BUILDING THE SEARCH BUTTON \\
-let button = $("<button>");
-button.addClass("btn-1");
-button.attr("data-name", "btn0");
-button.text("Search");
-$(".city").append(button);
-
-// SEARCH BUTTON LISTENER \\
-$(".btn-1").on("click", function () {
   // CURRENT CITY WEATHER \\
   $.ajax({
     url: cityQueryURL,
     method: "GET",
   }).then(function (response) {
     console.log(response);
+    currentTemp = Math.trunc((response.main.temp - 273.15) * 1.8 + 32);
+    humidity = response.main.humidity;
+    windSpeed = response.wind.speed;
+
+    console.log(humidity);
+    console.log(windSpeed);
   });
 
   // 2HR-5 DAY FORCAST \\
@@ -37,5 +29,24 @@ $(".btn-1").on("click", function () {
   }).then(function (response) {
     console.log(response);
   });
-  console.log($(".cityNameInput").val());
+}
+
+// BUILDING INPUT CITY ELEMENT \\
+let searchCityInputBox = $("<input>");
+searchCityInputBox.addClass("cityNameInput");
+searchCityInputBox.attr("value");
+$("#city").append(searchCityInputBox);
+
+// BUILDING THE SEARCH BUTTON \\
+let button = $("<button>");
+button.addClass("btn-1");
+button.attr("data-name", "btn0");
+button.text("Search");
+$("#city").append(button);
+
+// SEARCH BUTTON LISTENER \\
+
+$(".btn-1").on("click", function () {
+  let cityName = $(".cityNameInput").val();
+  weatherApp(cityName);
 });
