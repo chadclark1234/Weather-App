8ba67b5bd148fabaa255c502bdd987d74d99f088
Chad Clark <chadclark12@hotmail.com>
2021-01-25 16:39:50 -0600

    changed second API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  //TODAYS CURRENT DATE
+  // TODAYS CURRENT DATE
   let currentDate = moment().format("L");
 
-  //BUILD ARRAY FOR EACH ADDED CITY \\
+  // BUILD ARRAY FOR EACH ADDED CITY \\
   let citiesArr = JSON.parse(localStorage.getItem("citiesArr")) || [];
 
   let defaultCity = citiesArr[citiesArr.length - 1] ?? "Minneapolis";
@@ -15,10 +15,10 @@ $(document).ready(function () {
   // BUILD BUTTONS ON PAGE LOAD \\
   buildButtons();
 
-  //RUN API ON PAGE LOAD WITH LAST CITY \\
+  // RUN API ON PAGE LOAD WITH LAST CITY \\
   getWeather(defaultCity);
 
-  //SUBMIT BUTTON LISTENER \\
+  // SUBMIT BUTTON LISTENER \\
   $("#search-submit").on("click", function (event) {
     event.preventDefault();
 
@@ -32,26 +32,23 @@ $(document).ready(function () {
     // API CALL FUNCTION \\
     getWeather(cityName);
 
-    //BUILD CITY ARRAY \\
+    // BUILD CITY ARRAY \\
     citiesArr.push(cityName);
 
-    //LOCAL STORAGE
+    // LOCAL STORAGE \\
     localStorage.setItem("citiesArr", JSON.stringify(citiesArr));
 
-    //CALL FUNCTION TO ADD CITY BUTTONS \\
+    // CALL FUNCTION TO ADD CITY BUTTONS \\
     buildButtons();
   });
 
   // API FUNCTION \\
   function getWeather(cityName) {
-    //API KEY \\
+    // API KEY \\
     let APIKey = "3a623ea6ade278ada9b6b26990b8755d";
 
     // URL TO API FOR CURRENT WEATHER BY CITY \\
     let cityQueryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`;
-
-    // URL TO API FOR 2HR/5DAY FORECAST BY CITY \\
-    let fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
 
     // ONE DAY WEATHER API \\
     $.ajax({
@@ -61,32 +58,23 @@ $(document).ready(function () {
       lat = currentResponse.coord.lat;
       lon = currentResponse.coord.lon;
       // URL TO API FOR UV INDEX \\
-      const uvQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+      let fiveDayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIKey}`;
       // UV INDEX API \\
       $.ajax({
-        url: uvQueryURL,
+        url: fiveDayURL,
         method: "GET",
-      }).then(function (uvResponse) {
-        let currentUVindex = Math.trunc(uvResponse["value"]);
+      }).then(function (fiveResponse) {
+        let currentUVindex = fiveResponse.current.uvi.toFixed(1);
         currentUVdisplay(currentUVindex);
+        dayOne(fiveResponse);
+        dayTwo(fiveResponse);
+        dayThree(fiveResponse);
+        dayFour(fiveResponse);
+        dayFive(fiveResponse);
       });
 
       // CALL FUNCTION TO INSERT CURRENT CITY INFO \\
       insertCurrentInfo(currentResponse);
-    });
-
-    // FIVE DAY WEATHER API \\
-    $.ajax({
-      url: fiveDayURL,
-      method: "GET",
-    }).then(function (fiveResponse) {
-      // console.log(fiveResponse);
-      // 5 DAY BUILD FUNCTION CALLS \\
-      dayOne(fiveResponse);
-      dayTwo(fiveResponse);
-      dayThree(fiveResponse);
-      dayFour(fiveResponse);
-      dayFive(fiveResponse);
     });
 
     // CLEAR SEARCH TEXT BOX \\
@@ -105,7 +93,6 @@ $(document).ready(function () {
 
       // ADD CLASS TO BUTTON \\
       newCityButton.addClass("btn btn-primary new-city-button text-capitalize");
-      // newCityButton.addClass("btn");
 
       // ADD DATA- ATTRIBUTE \\
       newCityButton.attr("data-name", citiesArr[i]);
@@ -204,12 +191,11 @@ $(document).ready(function () {
 
   // DAY 1 OF FIVE DAY \\
   function dayOne(fiveResponse) {
-    let dayOneDate = fiveResponse.list[3]["dt_txt"];
-    let dayOneIcon = fiveResponse.list[3]["weather"][0]["icon"];
-    let dayOneDescription = fiveResponse.list[3]["weather"][0]["description"];
-    let dayOneTempK = fiveResponse.list[3]["main"]["temp"];
-    let dayOneHumidity = fiveResponse.list[3]["main"]["humidity"];
-    let dayOneTempF = newTempF(fiveResponse.list[3]["main"]["temp"]);
+    let dayOneIcon = fiveResponse.daily[0]["weather"][0]["icon"];
+    let dayOneDescription = fiveResponse.daily[0]["weather"][0]["description"];
+    let dayOneTempK = fiveResponse.daily[0]["temp"]["max"];
+    let dayOneHumidity = fiveResponse.daily[0]["humidity"];
+    let dayOneTempF = newTempF(dayOneTempK);
     $("#day-one-date").text(moment().add("1", "day").format("MM-DD-YYYY"));
     let dayOneIconString = `http://openweathermap.org/img/wn/${dayOneIcon}@2x.png`;
     $("#day-one-weather-image").attr("src", dayOneIconString);
@@ -218,14 +204,13 @@ $(document).ready(function () {
     $("#day-one-humidity").text(`H ${dayOneHumidity}%`);
   }
 
-  // DAY 2 OF FIVE DAY \\
+  // // DAY 2 OF FIVE DAY \\
   function dayTwo(fiveResponse) {
-    let dayTwoDate = fiveResponse.list[7]["dt_txt"];
-    let dayTwoIcon = fiveResponse.list[7]["weather"][0]["icon"];
-    let dayTwoDescription = fiveResponse.list[7]["weather"][0]["description"];
-    let dayTwoTempK = fiveResponse.list[7]["main"]["temp"];
-    let dayTwoHumidity = fiveResponse.list[7]["main"]["humidity"];
-    let dayTwoTempF = newTempF(fiveResponse.list[7]["main"]["temp"]);
+    let dayTwoIcon = fiveResponse.daily[1]["weather"][0]["icon"];
+    let dayTwoDescription = fiveResponse.daily[1]["weather"][0]["description"];
+    let dayTwoTempK = fiveResponse.daily[1]["temp"]["max"];
+    let dayTwoHumidity = fiveResponse.daily[1]["humidity"];
+    let dayTwoTempF = newTempF(dayTwoTempK);
     $("#day-two-date").text(moment().add("2", "day").format("MM-DD-YYYY"));
     let dayTwoIconString = `http://openweathermap.org/img/wn/${dayTwoIcon}@2x.png`;
     $("#day-two-weather-image").attr("src", dayTwoIconString);
@@ -234,15 +219,14 @@ $(document).ready(function () {
     $("#day-two-humidity").text(` ${dayTwoHumidity}%`);
   }
 
-  // DAY 3 OF FIVE DAY \\
+  // // DAY 3 OF FIVE DAY \\
   function dayThree(fiveResponse) {
-    let dayThreeDate = fiveResponse.list[15]["dt_txt"];
-    let dayThreeIcon = fiveResponse.list[15]["weather"][0]["icon"];
+    let dayThreeIcon = fiveResponse.daily[2]["weather"][0]["icon"];
     let dayThreeDescription =
-      fiveResponse.list[15]["weather"][0]["description"];
-    let dayThreeTempK = fiveResponse.list[15]["main"]["temp"];
-    let dayThreeHumidity = fiveResponse.list[15]["main"]["humidity"];
-    let dayThreeTempF = newTempF(fiveResponse.list[15]["main"]["temp"]);
+      fiveResponse.daily[2]["weather"][0]["description"];
+    let dayThreeTempK = fiveResponse.daily[2]["temp"]["max"];
+    let dayThreeHumidity = fiveResponse.daily[2]["humidity"];
+    let dayThreeTempF = newTempF(dayThreeTempK);
     $("#day-three-date").text(moment().add("3", "day").format("MM-DD-YYYY"));
     let dayThreeIconString = `http://openweathermap.org/img/wn/${dayThreeIcon}@2x.png`;
     $("#day-three-weather-image").attr("src", dayThreeIconString);
@@ -251,14 +235,13 @@ $(document).ready(function () {
     $("#day-three-humidity").text(`H ${dayThreeHumidity}%`);
   }
 
-  // DAY 4 OF FIVE DAY \\
+  // // DAY 4 OF FIVE DAY \\
   function dayFour(fiveResponse) {
-    let dayFourDate = fiveResponse.list[23]["dt_txt"];
-    let dayFourIcon = fiveResponse.list[23]["weather"][0]["icon"];
-    let dayFourDescription = fiveResponse.list[23]["weather"][0]["description"];
-    let dayFourTempK = fiveResponse.list[23]["main"]["temp"];
-    let dayFourHumidity = fiveResponse.list[23]["main"]["humidity"];
-    let dayFourTempF = newTempF(fiveResponse.list[23]["main"]["temp"]);
+    let dayFourIcon = fiveResponse.daily[3]["weather"][0]["icon"];
+    let dayFourDescription = fiveResponse.daily[3]["weather"][0]["description"];
+    let dayFourTempK = fiveResponse.daily[3]["temp"]["max"];
+    let dayFourHumidity = fiveResponse.daily[3]["humidity"];
+    let dayFourTempF = newTempF(dayFourTempK);
     $("#day-four-date").text(moment().add("4", "day").format("MM-DD-YYYY"));
     let dayFourIconString = `http://openweathermap.org/img/wn/${dayFourIcon}@2x.png`;
     $("#day-four-weather-image").attr("src", dayFourIconString);
@@ -267,14 +250,13 @@ $(document).ready(function () {
     $("#day-four-humidity").text(`H ${dayFourHumidity}%`);
   }
 
-  // DAY 5 OF FIVE DAY \\
+  // // DAY 5 OF FIVE DAY \\
   function dayFive(fiveResponse) {
-    let dayFiveDate = fiveResponse.list[31]["dt_txt"];
-    let dayFiveIcon = fiveResponse.list[31]["weather"][0]["icon"];
-    let dayFiveDescription = fiveResponse.list[31]["weather"][0]["description"];
-    let dayFiveTempK = fiveResponse.list[31]["main"]["temp"];
-    let dayFiveHumidity = fiveResponse.list[31]["main"]["humidity"];
-    let dayFiveTempF = newTempF(fiveResponse.list[31]["main"]["temp"]);
+    let dayFiveIcon = fiveResponse.daily[4]["weather"][0]["icon"];
+    let dayFiveDescription = fiveResponse.daily[4]["weather"][0]["description"];
+    let dayFiveTempK = fiveResponse.daily[4]["temp"]["max"];
+    let dayFiveHumidity = fiveResponse.daily[4]["humidity"];
+    let dayFiveTempF = newTempF(dayFiveTempK);
     $("#day-five-date").text(moment().add("5", "day").format("MM-DD-YYYY"));
     let dayFiveIconString = `http://openweathermap.org/img/wn/${dayFiveIcon}@2x.png`;
     $("#day-five-weather-image").attr("src", dayFiveIconString);
